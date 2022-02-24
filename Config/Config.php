c19c160657b06a43fbb654ce610303d42365ea48
kevin <kevinmarcas3@gmail.com>
2022-02-23 20:51:12 -0500

    conexion

--- Config/Config.php
+++ Config/Config.php
@@ -2,9 +2,9 @@
 
 const base_url = 'http://localhost/sistema_tareas/';
 const host = 'localhost';
-//const port = 33065;
+const port = 33065;
 const user = 'root';
-const pass = 'root';
+const pass = '';
 const db = 'tarea_sis';
 const charset = 'charset=utf8';
 
